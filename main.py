35e919488ad13f35a0349f91b380c8a9be81e1ae
Jacob Henn <windowWeasel@outlook.com>
2020-01-29 17:51:31 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,96 @@
+import json
+import recipeModule
+
+tree = json.loads(open('C:/Users/windo/Desktop/Stuff/Projects/Minecraft/birchTree/tree.txt', 'r').read().replace('\'', '\"'))
+
+inputList = []
+
+# The function that sustains the REPL
+def takeInput():
+    global inputList
+
+    inputList = input('[birchTree] ').split(' ')
+
+    exec(inputList[0] + '()')
+
+    takeInput()
+
+
+# pr <itemName>:
+#   print the ingredients used to craft <itemName>.
+def pr():
+    print(recipeModule.formatRecipe(recipeModule.ingredients(inputList[1])))
+
+
+# x:
+#   save the tree and exit the program
+def x():
+    open('C:/Users/windo/Desktop/Stuff/Projects/Minecraft/birchTree/tree.txt', 'w').write(str(tree))
+    exit()
+
+
+# xws:
+#   exit the program without saving changes
+def xws():
+    exit()
+
+
+# new <branchName>:
+#   create a new branch on the project tree called <projectName>
+def new():
+    doAtLevel(inputList[2].split('/'), update('{' + inputList[1] + ' : {}}'))
+
+
+# rm <branchName>:
+#   remove the branch named <branchName>
+def rm():
+    try:
+        exec('del(' + getLevel() + '[inputList[1]])')
+    except:
+        print('E2: Not a valid branch name')
+
+
+# pt:
+#   prints the current tree in raw form
+def pt():
+    print(tree)
+
+
+# rq <itemNumber> <itemName> <branchName>:
+#   specify that the project <branchName> requires <itemNumber> of <itemName>
+def rq():
+    recipe = getRecipe(inputList[1])
+
+    for item in recipe:
+        item = item.split(' ')
+        print(item)
+        item[0] = int(item[0])
+        print(item)
+        item[0] = str(item[0] * inputList[2])
+        print(item)
+        ' '.join(item)
+        print(item)
+
+    exec(getLevel() + '.update({inputList[1] : ' + str(recipe) + '})')
+
+
+# lr <branchName>:
+#   print all the requirements in <branchName>
+def lr():
+    print(getLevel())
+    testVar = 'test'
+    exec('%s = %d' % (testVar, getLevel())) 
+    print(testVar)
+
+    for item in exec(getLevel()):
+        print('\n'.join(item))
+
+
+# run <command>
+#   runs <command> as though it were python code
+def run():
+    exec(inputList[1])
+
+
+# start the REPL
+takeInput()
---
+++ recipeModule.py
@@ -0,0 +1,47 @@
+import json
+
+def uniqueElementsIn(inputList):
+    return(list(set(inputList)))
+
+def itemLabel(jsonParsed):
+    firstIngredient = list(jsonParsed['ingredients'][0].keys())[0]
+    return(firstIngredient)
+    
+
+def ingredients(itemName):
+    # Generate a file name based off of the item name
+    fileName = 'C:/Users/windo/Desktop/Stuff/Projects/Minecraft/birchTree/recipes/' + itemName + '.json'
+
+    # Read the file as json
+    try:
+        jsonRaw = open(fileName, 'r').read()
+    except:
+        jsonRaw = ''
+        print('E1: Not a valid item name')
+        return
+
+    jsonParsed = json.loads(jsonRaw)
+
+    # Find which of four variations the file is formatted in
+    recipeType = jsonParsed['type'].replace('minecraft:crafting_', '')
+    keyVersion = itemLabel(jsonParsed)
+
+    # If the format is one of the shapeless variety, form a material list
+    if recipeType == 'shapeless':
+        rawIngredients = []
+        numberedIngredients = []
+
+        for value in jsonParsed['ingredients']:
+            rawIngredients.append(value[keyVersion])
+
+        for value in uniqueElementsIn(rawIngredients):
+            numberedIngredients.append([rawIngredients.count(value), value.replace('minecraft:', '')])
+
+        return(numberedIngredients)
+
+def formatRecipe(rawRecipe):
+    formattedRecipe = []
+    for item in rawRecipe:
+        formattedRecipe.append(str(item[0]) + ' ' + item[1])
+
+    return('\n'.join(formattedRecipe))
